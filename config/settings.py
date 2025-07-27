3df88967f14063ccc40abff6bd57b6ce0e90e1b3
Daniel <daniel.stevens@ephcreative.co.uk>
2025-07-27 14:13:47 +0100

    add apps

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+
+    'accounts',
+    'drinks',
+    'sessions_app',
+    'consumption',
+    'kiosk',
+    'scoreboard',
 ]
 
 MIDDLEWARE = [
